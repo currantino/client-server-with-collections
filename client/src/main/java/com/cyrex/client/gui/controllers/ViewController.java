6d34b0be242760f6361656e9aa8f7ed201852f6b
Matvey Smorodin <mat.smooooder@gmail.com>
2022-06-16 06:37:25 +0300

    switchToMainView method added to ViewController

--- client/src/main/java/com/cyrex/client/gui/controllers/ViewController.java
+++ client/src/main/java/com/cyrex/client/gui/controllers/ViewController.java
@@ -1,8 +1,10 @@
 package com.cyrex.client.gui.controllers;
 
 import com.cyrex.client.gui.views.LoginView;
+import com.cyrex.client.gui.views.MainView;
 import com.cyrex.client.gui.views.RegisterView;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,8 +13,8 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ViewController{
-    public static void switchToRegisterView(Button btn) {
-        ((Stage)btn.getScene().getWindow()).close();
+    public static void switchToRegisterView(Node node) {
+        ((Stage)node.getScene().getWindow()).close();
         try {
             Parent root = FXMLLoader.load(RegisterView.class.getResource("/com.cyrex.client.gui/register.fxml"));
             Stage stage = new Stage();
@@ -27,10 +29,26 @@ public class ViewController{
         }
     }
 
-    public static void switchToLoginView(Button btn) {
-        ((Stage)btn.getScene().getWindow()).close();
+    public static void switchToLoginView(Node node) {
+        ((Stage)node.getScene().getWindow()).close();
         try {
             Parent root = FXMLLoader.load(RegisterView.class.getResource("/com.cyrex.client.gui/login.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root, 600, 400);
+            stage.setTitle("Login");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void switchToMainView(Node node) {
+        ((Stage)node.getScene().getWindow()).close();
+        try {
+            Parent root = FXMLLoader.load(MainView.class.getResource("/com.cyrex.client.gui/main.fxml"));
             Stage stage = new Stage();
             Scene scene = new Scene(root, 600, 400);
             stage.setTitle("Login");
